6115e2f90f2181f48bd74c7f9301aeadb8f9e3c2
gaurav <gauravkad139@gmail.com>
2024-04-22 20:21:39 +0530

    Deserialization

--- src/E_Commerce_APITest/Ecom_API_Test.java
+++ src/E_Commerce_APITest/Ecom_API_Test.java
@@ -1,13 +1,27 @@
 package E_Commerce_APITest;
 
+import Pojo.LoginRequest;
 import io.restassured.builder.RequestSpecBuilder;
+import io.restassured.http.ContentType;
 import io.restassured.specification.RequestSpecification;
+import static io.restassured.RestAssured.given;
+
 
 public class Ecom_API_Test {
 
 	public static void main(String[] args) {
-		RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").build();
-
+		RequestSpecification reqSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").setContentType(ContentType.JSON).build();
+		
+		//Call Pojo Class Object
+		
+		LoginRequest loginRequest = new LoginRequest();
+		loginRequest.setUserEmail("123456gk@gmail.com");
+		loginRequest.setUserPassword("1234@Abcd");
+		
+		RequestSpecification reqLogin = given().spec(reqSpec).body(loginRequest);
+		
+		reqLogin.when().post("/api/ecom/auth/login");
+		
 
 	}
 
---
+++ src/Pojo/LoginRequest.java
@@ -0,0 +1,22 @@
+package Pojo;
+
+public class LoginRequest {
+	
+	private String userEmail;
+	private String userPassword;
+	
+	//Getters and Setters - Alt + shift + S
+	public String getUserEmail() {
+		return userEmail;
+	}
+	public void setUserEmail(String userEmail) {
+		this.userEmail = userEmail;
+	}
+	public String getUserPassword() {
+		return userPassword;
+	}
+	public void setUserPassword(String userPassword) {
+		this.userPassword = userPassword;
+	}
+
+}
