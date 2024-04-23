ecdaa000248b0fdd4e5598e01c75d24dbfa23bba
gaurav <gauravkad139@gmail.com>
2024-04-23 10:59:32 +0530

    Create New Product

--- src/E_Commerce_APITest/Ecom_API_Test.java
+++ src/E_Commerce_APITest/Ecom_API_Test.java
@@ -4,8 +4,11 @@ import Pojo.LoginRequest;
 import Pojo.LoginResponse;
 import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
+import io.restassured.path.json.JsonPath;
 import io.restassured.specification.RequestSpecification;
 import static io.restassured.RestAssured.given;
+
+import java.io.File;
 
 
 public class Ecom_API_Test {
@@ -23,10 +26,41 @@ public class Ecom_API_Test {
 		
 		LoginResponse loginresponse = reqLogin.when().post("/api/ecom/auth/login").then().extract().response().as(LoginResponse.class);
 		String token = loginresponse.getToken();
+		String userID = loginresponse.getUserId();
 		
-		System.out.print(token);
+		System.out.println("Authorization Token => " + token);
+		System.out.println("User ID  => " + userID);
 		
-
+		String NewProductImgPath ="F:\\Selenium\\E-Commerce_APITest_RestAssured\\laptop.jpg";
+		
+		//Create Product 
+		RequestSpecification createProductBaseSpec = new RequestSpecBuilder().setBaseUri("https://rahulshettyacademy.com").addHeader("Authorization", token).build();
+		
+		//send body as Form Data 
+		RequestSpecification createProductAddSpec=
+		given().spec(createProductBaseSpec)
+		.param("productName","Laptop")
+		.param("productAddedBy",userID )
+		.param("productCategory", "Electric")
+		.param("productSubCategory", "Ipad")
+		.param("productPrice", "12000")
+		.param("productDescription","Laptop")
+		.param("productFor", "men")
+		.multiPart("productImage", new File(NewProductImgPath));
+		
+		//user MultiPart to send Files
+		
+		 String newProductResponse = createProductAddSpec.when().post("/api/ecom/product/add-product")
+		.then().extract().response().asString();
+		 
+		 JsonPath js = new JsonPath(newProductResponse);
+		
+		String productId = js.get("productId");
+		String message = js.get("messgae");
+		System.out.println("New Product Id => "+ productId);
+		System.out.println("Message => "+ message);
+		
+		
 	}
 
 }
